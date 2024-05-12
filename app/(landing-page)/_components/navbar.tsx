6e48aa24a300aa601e6af5f776605d8271462ee7
choiruladamm <choiruladamm@gmail.com>
2024-05-12 17:10:26 +0700

    chore(deps): add next-themes

--- app/(landing-page)/_components/navbar.tsx
+++ app/(landing-page)/_components/navbar.tsx
@@ -1,11 +1,28 @@
-'use client'
+'use client';
 
+import { useScrollTop } from '@/hooks/use-scroll-top';
+import { cn } from '@/lib/utils';
 import React, { FC } from 'react';
+import { Logo } from '.';
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
-  return <div>Navbar</div>;
+  const scrolled = useScrollTop();
+
+  return (
+    <div
+      className={cn(
+        'z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6',
+        scrolled && 'border-b shadow-sm'
+      )}
+    >
+      <Logo />
+      <div className='flex md:ml-auto md:justify-end justify-between w-full items-center gap-x-2'>
+        Login
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
