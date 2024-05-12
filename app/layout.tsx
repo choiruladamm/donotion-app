e5fa80ca8543a75803501bf608da79ed0a6133a2
choiruladamm <choiruladamm@gmail.com>
2024-05-12 19:15:08 +0700

    feat: add convex with clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/providers/theme-provider';
+import ConvexProvider from '@/providers/convex-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,15 +33,17 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-          storageKey='donotion-theme-2'
-        >
-          {children}
-        </ThemeProvider>
+        <ConvexProvider>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+            storageKey='donotion-theme-2'
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexProvider>
       </body>
     </html>
   );
