cf47a9903abad19dd2590acdd07ee5551dd610c1
choiruladamm <choiruladamm@gmail.com>
2024-05-14 22:01:36 +0700

    feat: add document list item

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -1,22 +1,91 @@
 'use client';
 
+import { api } from '@/convex/_generated/api';
 import { Doc, Id } from '@/convex/_generated/dataModel';
-import React, { FC } from 'react';
+import { useQuery } from 'convex/react';
+import { useParams, useRouter } from 'next/navigation';
+import React, { FC, useState } from 'react';
+import Item from './item';
+import { cn } from '@/lib/utils';
+import { FileIcon } from 'lucide-react';
 
 interface DocumentListProps {
   parentDocumentId?: Id<'documents'>;
-  lavel?: number;
+  level?: number;
   data?: Doc<'documents'>;
 }
 
 const DocumentList: FC<DocumentListProps> = ({
   parentDocumentId,
-  lavel = 0,
+  level = 0,
   data,
 }) => {
+  const params = useParams();
+  const router = useRouter();
+  const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+
+  const onExpanded = (documentId: string) => {
+    setExpanded((prevExpanded) => ({
+      ...prevExpanded,
+      [documentId]: !prevExpanded[documentId],
+    }));
+  };
+
+  const documents = useQuery(api.documents.getSidebar, {
+    parentDocument: parentDocumentId,
+  });
+
+  const onRedirect = (documentId: string) => {
+    router.push(`/documents/${documentId}`);
+  };
+
+  if (documents === undefined) {
+    return (
+      <>
+        <Item.Skeleton level={level} />
+        {level === 0 && (
+          <>
+            <Item.Skeleton level={level} />
+            <Item.Skeleton level={level} />
+          </>
+        )}
+      </>
+    );
+  }
+
   return (
-    <div></div>
-  )
+    <>
+      <p
+        className={cn(
+          `hidden text-sm font-medium text-muted-foreground/80`,
+          expanded && 'last:block',
+          level === 0 && 'hidden'
+        )}
+        style={{ paddingLeft: level ? `${level * 12 + 25}px` : undefined }}
+      >
+        No pages available
+      </p>
+
+      {documents.map((document) => (
+        <div key={document._id}>
+          <Item
+            id={document._id}
+            onClick={() => onRedirect(document._id)}
+            label={document.title}
+            icon={FileIcon}
+            documentIcon={document.icon}
+            active={params.documentId === document._id}
+            level={level}
+            onExpand={() => onExpanded(document._id)}
+            expanded={expanded[document._id]}
+          />
+          {expanded[document._id] && (
+            <DocumentList parentDocumentId={document._id} level={level + 1} />
+          )}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default DocumentList;
--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -3,7 +3,7 @@
 import { Skeleton } from '@/components/ui/skeleton';
 import { Id } from '@/convex/_generated/dataModel';
 import { cn } from '@/lib/utils';
-import { ChevronsDown, ChevronsRight, LucideIcon } from 'lucide-react';
+import { ChevronDown, ChevronRight, LucideIcon } from 'lucide-react';
 import React, { FC } from 'react';
 
 interface ItemProps {
@@ -31,7 +31,7 @@ const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
-  const ChevronIcon = expanded ? ChevronsDown : ChevronsRight;
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   const handleExpand = (event: React.MouseEvent<HTMLDivElement>) => {
     event.stopPropagation();
@@ -40,7 +40,7 @@ const Item = ({
 
   return (
     <div
-      role='button'
+      role="button"
       onClick={onClick}
       style={{
         paddingLeft: level ? `${level * 12 + 12}px` : '12px',
@@ -52,25 +52,25 @@ const Item = ({
     >
       {!!id && (
         <div
-          className='h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1'
+          className="h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1"
           onClick={handleExpand}
-          role='button'
+          role="button"
         >
-          <ChevronIcon className='w-4 h-4 shrink-0 text-muted-foreground/50' />
+          <ChevronIcon className="w-4 h-4 shrink-0 text-muted-foreground/50" />
         </div>
       )}
       {documentIcon ? (
-        <div className='shrink-0 mr-2 text-[18px]'>{documentIcon}</div>
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
       ) : (
-        <Icon className='shrink-0 size[18px] mr-2 text-muted-foreground' />
+        <Icon className="shrink-0 size[18px] mr-2 text-muted-foreground" />
       )}
-      <span className='truncate'>{label}</span>
+      <span className="truncate">{label}</span>
       {isSearch && (
         <kbd
-          className='ml-auto pointer-events-none inline-flex gap-1 items-center h-5 select-none rounded border
-        bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'
+          className="ml-auto pointer-events-none inline-flex gap-1 items-center h-5 select-none rounded border
+        bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100"
         >
-          <span className='text-xs'>CTRL</span>K
+          <span className="text-xs">CTRL</span>K
         </kbd>
       )}
     </div>
@@ -82,11 +82,11 @@ export default Item;
 Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
   return (
     <div
-      className='flex gap-x-2 py-[3px]'
+      className="flex gap-x-2 py-[3px]"
       style={{ paddingLeft: level ? `${level * 12 + 25}px` : '12px' }}
     >
-      <Skeleton className='w-4 h-4' />
-      <Skeleton className='w-4 h-[30%]' />
+      <Skeleton className="w-4 h-4" />
+      <Skeleton className="w-4 h-[30%]" />
     </div>
   );
 };
