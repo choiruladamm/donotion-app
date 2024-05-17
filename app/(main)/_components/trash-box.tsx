4dbad016f5e31a0cfb91af484a72bf55aaf0b511
choiruladamm <choiruladamm@gmail.com>
2024-05-17 22:37:49 +0700

    feat: add input component

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,11 +1,31 @@
 'use client';
 
+import { Spinner } from '@/components/spinner';
+import { api } from '@/convex/_generated/api';
+import { useMutation, useQuery } from 'convex/react';
+import { useParams, useRouter } from 'next/navigation';
 import React, { FC } from 'react';
 
 interface TrashBoxProps {}
 
 const TrashBox: FC<TrashBoxProps> = ({}) => {
-  return <div>TrashBox</div>;
+  const router = useRouter();
+  const params = useParams();
+  const documents = useQuery(api.documents.getTrash);
+  const restore = useMutation(api.documents.restore);
+  const remove = useMutation(api.documents.remove);
+
+  if (documents === undefined) {
+    return (
+      <div className="flex h-full items-center justify-center p-4">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  return <div className='text-sm'>
+    
+  </div>;
 };
 
 export default TrashBox;
