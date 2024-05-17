08d492379967012d375d6e9003340d9e5ec00cf7
choiruladamm <choiruladamm@gmail.com>
2024-05-17 23:03:49 +0700

    feat: wip search command component

---
+++ components/search-command.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React, { FC } from 'react';
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
+
+interface SearchCommandProps {}
+
+const SearchCommand: FC<SearchCommandProps> = ({}) => {
+  return <div>SearchCommand</div>;
+};
+
+export default SearchCommand;
