d7dd0fb5424730ca699b69b70e0c2eba0d269d70
choiruladamm <choiruladamm@gmail.com>
2024-05-18 23:58:48 +0700

    fix: command dialog error jsx indetifier

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -64,7 +64,10 @@ const SearchCommand: FC<SearchCommandProps> = ({}) => {
               onSelect={onSelect}
             >
               {document.icon ? (
-              
+                <p className="mr-2 text-[18px]">{document.icon}</p>
+              ) : (
+                <File className="size-4 mr-2" />
+              )}
               <span>{document.title}</span>
             </CommandItem>
           ))}
