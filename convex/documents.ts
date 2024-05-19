e2767ba59550b2b6a01afbc569c98e82ee6beb89
choiruladamm <choiruladamm@gmail.com>
2024-05-19 15:59:18 +0700

    feat: add remove cover image schema

--- convex/documents.ts
+++ convex/documents.ts
@@ -319,3 +319,32 @@ export const removeIcon = mutation({
     return document;
   },
 });
+
+export const removeCoverImage = mutation({
+  args: { id: v.id('documents') },
+  handler: async (context, args) => {
+    const identity = await context.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Unauthenticated');
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await context.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error('Not found');
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const document = await context.db.patch(args.id, {
+      coverImage: undefined,
+    });
+
+    return document;
+  },
+});
