812960990ddde24eaae6ce5b11ffe94c78113bdc
choiruladamm <choiruladamm@gmail.com>
2024-05-19 15:28:21 +0700

    chore(deps): add @edgestore/react

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -6,6 +6,7 @@ import { useMutation } from 'convex/react';
 import { useParams } from 'next/navigation';
 import React, { FC, useState } from 'react';
 import { Dialog, DialogContent, DialogHeader } from '@/components/ui/dialog';
+import { SingleImageDropzone } from '../single-image-dropzone';
 
 interface CoverImageModalProps {}
 
@@ -18,12 +19,40 @@ const CoverImageModal: FC<CoverImageModalProps> = ({}) => {
 
   const coverImage = useCoverImage();
 
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const response = await edgestore.publicFiles.upload({
+        file,
+        options: {
+          replaceTargetUrl: coverImage.url,
+        },
+      });
+
+      await update({
+        id: params.documentId as Id<'documents'>,
+        coverImage: response.url,
+      });
+
+      onClose();
+    }
+  };
+
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
       <DialogContent>
         <DialogHeader>
           <h2 className="text-center text-lg font-semibold">Cover Image</h2>
         </DialogHeader>
+
+        <SingleImageDropzone
+          className="w-full outline-none"
+          disabled={isSubmitting}
+          value={file}
+          onChange={onChange}
+        />
       </DialogContent>
     </Dialog>
   );
